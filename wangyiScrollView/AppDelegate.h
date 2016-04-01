85a6866901679c7c22940c112851f9525bb36ecd
macmini <macmini@macmini.local>
2016-04-01 10:05:50 +0800

    Initial Commit

---
+++ wangyiScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  wangyiScrollView
+//
+//  Created by macmini on 16/4/1.
+//  Copyright © 2016年 macmini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ wangyiScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  wangyiScrollView
+//
+//  Created by macmini on 16/4/1.
+//  Copyright © 2016年 macmini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
